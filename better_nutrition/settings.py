e7c0c0bfc0dfeb7d99d93e910e8a43b9ba8fb4ea
LekishviliRati <lekishvili.rati@gmail.com>
2021-07-22 00:32:05 +0200

    Hide Secret Key

--- better_nutrition/settings.py
+++ better_nutrition/settings.py
@@ -21,11 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u2zc5rx%p6rvf6ds-&d_)1%un)^s+1pou!@=*ilh!@*5rbldpd'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Only if you are working in dev environment
+# DEBUG = True
+
+# Must be turned False in production
+DEBUG = False
 
 ALLOWED_HOSTS = ['betternutrition.herokuapp.com']
 
